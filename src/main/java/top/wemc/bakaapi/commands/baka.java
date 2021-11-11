acd2aba00081b0ed309c291a90689ab618bf1065
WYH2004 <vipwyh2004@163.com>
2021-11-12 07:01:07 +0800

    添加 Debug 指令

--- src/main/java/top/wemc/bakaapi/commands/baka.java
+++ src/main/java/top/wemc/bakaapi/commands/baka.java
@@ -4,6 +4,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import top.wemc.bakaapi.Main;
+import top.wemc.bakaapi.config.MysqlConfig;
 
 /**
  * @author WYH2004
@@ -23,6 +24,27 @@ public class baka implements CommandExecutor {
                 if (commandSender.hasPermission("baka.admin.core.reload")){
                     Main.reloadConfigs();
                     commandSender.sendMessage("[BakaAPI] 配置文件已重载");
+                }
+            }
+            if (strings[0].equals("debug")){
+                if (commandSender.hasPermission("baka.admin.core.debug")){
+                    commandSender.sendMessage("[BakaAPI] /baka debug <mysql>");
+                }
+            }
+        }
+        if (strings.length == 2){
+            if (strings[0].equals("debug")){
+                if (strings[1].equals("mysql")){
+                    if (commandSender.hasPermission("baka.admin.core.debug.mysql")){
+                        if (MysqlConfig.debugMode){
+                            MysqlConfig.debugMode = false;
+                            commandSender.sendMessage("[BakaAPI] Mysql信息输出模式: INFO");
+                        }else {
+                            MysqlConfig.debugMode = true;
+                            commandSender.sendMessage("[BakaAPI] Mysql信息输出模式: DEBUG");
+                        }
+                        MysqlConfig.config.save();
+                    }
                 }
             }
         }
