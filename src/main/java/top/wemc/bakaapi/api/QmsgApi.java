f5813e28df6aaf3471f1bc020c696bfd98273a82
WYH2004 <vipwyh2004@163.com>
2021-11-22 13:17:27 +0800

    实现 QmsgApi

---
+++ src/main/java/top/wemc/bakaapi/api/QmsgApi.java
@@ -0,0 +1,65 @@
+package top.wemc.bakaapi.api;
+
+import com.google.gson.JsonElement;
+import top.wemc.bakaapi.config.PushConfig;
+import top.wemc.bakaapi.utils.JsonUtils;
+
+import java.io.IOException;
+
+/**
+ * @author WYH2004
+ * @date 2021/11/22 12:05
+ **/
+public class QmsgApi {
+    /**
+     * Qmsg酱 发送私聊消息
+     * @param msg
+     * @param qq
+     * @return boolean
+     * @throws IOException
+     */
+    public static boolean sendMassage(String msg,String ...qq) throws IOException {
+        if (PushConfig.qmsgEnabled){
+            String url = "https://qmsg.zendee.cn/send/" + PushConfig.qmsgToken + "?msg=" + msg;
+            if(qq.length > 0){
+                StringBuilder stringBuilder = new StringBuilder(url + "?qq=");
+                for (int i=0; i<qq.length; i++){
+                    if (i != 0){
+                        stringBuilder.append(",");
+                    }
+                    stringBuilder.append(qq[i]);
+                }
+                url = stringBuilder.toString();
+            }
+            JsonElement rawData = JsonUtils.getJsonURL(url,"success");
+            return rawData.getAsBoolean();
+        }
+        return false;
+    }
+
+    /**
+     * Qmsg酱 发送群聊消息(qq参数填入'-1'，则为群发)
+     * @param msg
+     * @param qq
+     * @return boolean
+     * @throws IOException
+     */
+    public static boolean sendGroupMassage(String msg,String ...qq) throws IOException {
+        if (PushConfig.qmsgEnabled){
+            String url = "https://qmsg.zendee.cn/group/" + PushConfig.qmsgToken + "?msg=" + msg;
+            if(qq.length > 0){
+                StringBuilder stringBuilder = new StringBuilder(url + "?qq=");
+                for (int i=0; i<qq.length; i++){
+                    if (i != 0){
+                        stringBuilder.append(",");
+                    }
+                    stringBuilder.append(qq[i]);
+                }
+                url = stringBuilder.toString();
+            }
+            JsonElement rawData = JsonUtils.getJsonURL(url,"success");
+            return rawData.getAsBoolean();
+        }
+        return false;
+    }
+}
